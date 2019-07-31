21b3c3dd3356f410fa8878bb99bca790a54fec7a
myname21-meet <myname21@meet.mit.edu>
2019-07-31 05:15:57 -0400

    a

--- databases.py
+++ databases.py
@@ -11,7 +11,7 @@ session = DBSession()
 def add_user(name,secret_word):
     """Add a user to the DB."""
     user = User(username=name)
-    #there is a line of code missing here, what else does a user need?
+    user.hash_password(secret_word)
     session.add(user)
     session.commit()
 
